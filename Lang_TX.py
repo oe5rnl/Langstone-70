494386a31e3e6de5296792ff30b0dffb8ca2c0eb
Reinhold <oe5rnl@oevsv.at>
2020-05-26 13:05:36 +0100

    1te

---
+++ Lang_TX.py
@@ -0,0 +1,228 @@
+#!/usr/bin/env python2
+# -*- coding: utf-8 -*-
+##################################################
+# GNU Radio Python Flow Graph
+# Title: Lang Tx
+# Generated: Tue May 26 10:13:50 2020
+##################################################
+
+
+from gnuradio import analog
+from gnuradio import audio
+from gnuradio import blocks
+from gnuradio import eng_notation
+from gnuradio import filter
+from gnuradio import gr
+from gnuradio import iio
+from gnuradio.eng_option import eng_option
+from gnuradio.filter import firdes
+from optparse import OptionParser
+import os
+import errno
+
+class Lang_TX(gr.top_block):
+
+    def __init__(self):
+        gr.top_block.__init__(self, "Lang Tx")
+
+        ##################################################
+        # Variables
+        ##################################################
+        self.USB = USB = False
+        self.PTT = PTT = True
+        self.NCW = NCW = False
+        self.MicGain = MicGain = 5.0
+        self.KEY = KEY = False
+        self.FMMIC = FMMIC = 50
+        self.FM = FM = True
+        self.CW = CW = False
+
+        ##################################################
+        # Blocks
+        ##################################################
+        self.rational_resampler_xxx_0 = filter.rational_resampler_ccc(
+                interpolation=12,
+                decimation=1,
+                taps=None,
+                fractional_bw=None,
+        )
+        self.pluto_sink_0 = iio.pluto_sink('ip:pluto.local', 2400000000, 529200, 2000000, 0x800, False, 0, '', True)
+        self.blocks_mute_xx_0_0 = blocks.mute_cc(bool(not PTT))
+        self.blocks_multiply_xx_0 = blocks.multiply_vcc(1)
+        self.blocks_multiply_const_vxx_4 = blocks.multiply_const_vcc((not FM, ))
+        self.blocks_multiply_const_vxx_3 = blocks.multiply_const_vcc((FM, ))
+        self.blocks_multiply_const_vxx_0_0 = blocks.multiply_const_vff((FMMIC/10.0, ))
+        self.blocks_multiply_const_vxx_0 = blocks.multiply_const_vff(((MicGain/10.0)*(not CW), ))
+        self.blocks_float_to_complex_0 = blocks.float_to_complex(1)
+        self.blocks_add_xx_2 = blocks.add_vcc(1)
+        self.blocks_add_xx_0 = blocks.add_vff(1)
+        self.band_pass_filter_1 = filter.fir_filter_fff(1, firdes.band_pass(
+        	1, 44100, 200, 3000, 100, firdes.WIN_HAMMING, 6.76))
+        self.band_pass_filter_0_0 = filter.fir_filter_ccc(1, firdes.complex_band_pass(
+        	1, 44100, -3000+USB*3300+CW*250, -300+USB*3300-CW*1950, 100, firdes.WIN_HAMMING, 6.76))
+        self.audio_source_0 = audio.source(44100, "hw:CARD=Device,DEV=0", False)
+        self.analog_sig_source_x_1_0 = analog.sig_source_f(44100, analog.GR_COS_WAVE, 800, int(CW and KEY), 0)
+        self.analog_sig_source_x_0 = analog.sig_source_c(44100, analog.GR_COS_WAVE, 0, 1, 0)
+        self.analog_nbfm_tx_0 = analog.nbfm_tx(
+        	audio_rate=44100,
+        	quad_rate=44100,
+        	tau=75e-6,
+        	max_dev=3000,
+        	fh=-1,
+                )
+        self.analog_const_source_x_0 = analog.sig_source_f(0, analog.GR_CONST_WAVE, 0, 0, 0)
+        self.analog_agc2_xx_1 = analog.agc2_cc(1e-1, 1e-1, 1.3, 1.0)
+        self.analog_agc2_xx_1.set_max_gain(10)
+
+
+
+        ##################################################
+        # Connections
+        ##################################################
+        self.connect((self.analog_agc2_xx_1, 0), (self.band_pass_filter_0_0, 0))
+        self.connect((self.analog_const_source_x_0, 0), (self.blocks_float_to_complex_0, 1))
+        self.connect((self.analog_nbfm_tx_0, 0), (self.blocks_multiply_const_vxx_3, 0))
+        self.connect((self.analog_sig_source_x_0, 0), (self.blocks_multiply_xx_0, 1))
+        self.connect((self.analog_sig_source_x_1_0, 0), (self.blocks_add_xx_0, 1))
+        self.connect((self.audio_source_0, 0), (self.blocks_multiply_const_vxx_0, 0))
+        self.connect((self.audio_source_0, 0), (self.blocks_multiply_const_vxx_0_0, 0))
+        self.connect((self.band_pass_filter_0_0, 0), (self.blocks_multiply_const_vxx_4, 0))
+        self.connect((self.band_pass_filter_1, 0), (self.analog_nbfm_tx_0, 0))
+        self.connect((self.blocks_add_xx_0, 0), (self.blocks_float_to_complex_0, 0))
+        self.connect((self.blocks_add_xx_2, 0), (self.rational_resampler_xxx_0, 0))
+        self.connect((self.blocks_float_to_complex_0, 0), (self.blocks_multiply_xx_0, 0))
+        self.connect((self.blocks_multiply_const_vxx_0, 0), (self.blocks_add_xx_0, 0))
+        self.connect((self.blocks_multiply_const_vxx_0_0, 0), (self.band_pass_filter_1, 0))
+        self.connect((self.blocks_multiply_const_vxx_3, 0), (self.blocks_add_xx_2, 0))
+        self.connect((self.blocks_multiply_const_vxx_4, 0), (self.blocks_add_xx_2, 1))
+        self.connect((self.blocks_multiply_xx_0, 0), (self.analog_agc2_xx_1, 0))
+        self.connect((self.blocks_mute_xx_0_0, 0), (self.pluto_sink_0, 0))
+        self.connect((self.rational_resampler_xxx_0, 0), (self.blocks_mute_xx_0_0, 0))
+
+    def get_USB(self):
+        return self.USB
+
+    def set_USB(self, USB):
+        self.USB = USB
+        self.band_pass_filter_0_0.set_taps(firdes.complex_band_pass(1, 44100, -3000+self.USB*3300+self.CW*250, -300+self.USB*3300-self.CW*1950, 100, firdes.WIN_HAMMING, 6.76))
+
+    def get_PTT(self):
+        return self.PTT
+
+    def set_PTT(self, PTT):
+        self.PTT = PTT
+        self.blocks_mute_xx_0_0.set_mute(bool(not self.PTT))
+
+    def get_NCW(self):
+        return self.NCW
+
+    def set_NCW(self, NCW):
+        self.NCW = NCW
+
+    def get_MicGain(self):
+        return self.MicGain
+
+    def set_MicGain(self, MicGain):
+        self.MicGain = MicGain
+        self.blocks_multiply_const_vxx_0.set_k(((self.MicGain/10.0)*(not self.CW), ))
+
+    def get_KEY(self):
+        return self.KEY
+
+    def set_KEY(self, KEY):
+        self.KEY = KEY
+        self.analog_sig_source_x_1_0.set_amplitude(int(self.CW and self.KEY))
+
+    def get_FMMIC(self):
+        return self.FMMIC
+
+    def set_FMMIC(self, FMMIC):
+        self.FMMIC = FMMIC
+        self.blocks_multiply_const_vxx_0_0.set_k((self.FMMIC/10.0, ))
+
+    def get_FM(self):
+        return self.FM
+
+    def set_FM(self, FM):
+        self.FM = FM
+        self.blocks_multiply_const_vxx_4.set_k((not self.FM, ))
+        self.blocks_multiply_const_vxx_3.set_k((self.FM, ))
+
+    def get_CW(self):
+        return self.CW
+
+    def set_CW(self, CW):
+        self.CW = CW
+        self.blocks_multiply_const_vxx_0.set_k(((self.MicGain/10.0)*(not self.CW), ))
+        self.band_pass_filter_0_0.set_taps(firdes.complex_band_pass(1, 44100, -3000+self.USB*3300+self.CW*250, -300+self.USB*3300-self.CW*1950, 100, firdes.WIN_HAMMING, 6.76))
+        self.analog_sig_source_x_1_0.set_amplitude(int(self.CW and self.KEY))
+
+
+def docommands(tb):
+  try:
+    os.mkfifo("/tmp/langstoneTx")
+  except OSError as oe:
+    if oe.errno != errno.EEXIST:
+      raise    
+  ex=False
+  lastbase=0
+  while not ex:
+    fifoin=open("/tmp/langstoneTx",'r')
+    while True:
+       try:
+        with fifoin as filein:
+         for line in filein:
+           line=line.strip()
+           if line=='Q':
+              ex=True        
+           if line=='R':
+              tb.set_PTT(False) 
+           if line=='T':
+              tb.set_PTT(True)
+           if line=='U':
+              tb.set_USB(True)
+              tb.set_FM(False)
+              tb.set_CW(False)              
+           if line=='L':
+              tb.set_USB(False)
+              tb.set_FM(False)
+              tb.set_CW(False) 
+           if line=='F':
+              tb.set_FM(True)             
+           if line=='C':
+              tb.set_CW(True)
+              tb.set_FM(False)
+              tb.set_USB(True) 
+           if line=='N':
+              tb.set_NCW(True)
+           if line=='W':
+              tb.set_NCW(False) 
+           if line=='K':
+              tb.set_KEY(True) 
+           if line=='k':
+              tb.set_KEY(False)    
+           if line[0]=='G':
+              value=int(line[1:])
+              tb.set_MicGain(value) 
+           if line[0]=='g':
+              value=int(line[1:])
+              tb.set_FMMIC(value)
+           if line=='H':
+              tb.lock()
+           if line=='h':
+              tb.unlock()                      
+       except:
+         break
+
+
+def main(top_block_cls=Lang_TX, options=None):
+
+    tb = top_block_cls()
+    tb.start()
+    docommands(tb)
+    tb.stop()
+    tb.wait()
+
+
+if __name__ == '__main__':
+    main()
---
+++ README.md
@@ -0,0 +1,102 @@
+# 70cm SDR Transceiver based on g4eml Langstone 
+
+-----------------------------
+- based on Longstone TRX
+- 500 kHz Bandwidth
+- fcenter = 438.712 MHz
+    works only on 70cm !!!
+    no other bands yet
+- Frequency marker
+- todo:
+-  test TX
+-  stop edges for frequency marker
+-----------------------------
+
+This is an experimental project to produce a simple VHF, UHF and Microwave SDR Transceiver operating on SSB CW and FM.
+
+It was inspired by the very successful Portsdown Amateur Television system created by the British Amateur Television Club.
+
+This is not yet a finished project but more of a demonstration of what can be done. 
+
+Currently only one set of hardware is supported:-
+
+- Raspberry Pi 4
+
+- Official Raspberry Pi 7" touchscreen.
+
+- Adalm Pluto SDR Module
+
+- USB Audio module. Connected to loudspeaker or headphones and microphone. 
+ 
+- USB Scroll mouse
+
+- PTT via Raspberry Pi GPIO pin 11. This needs a pull up resistor to 3.3V. Grounding this pin will switch to Transmit.
+
+- CW Key is via Raspberry Pi GPIO pin 12. This needs a pull up resistor to 3.3V. Grounding this pin will key the transmitter. 
+
+- Tx Output is via Raspberry Pi GPIO pin 40. This output goes high when the Langstone is transmitting. This can be used to switch antenna relays and amplifiers. (100ms delay included for sequencing)
+
+- 4 Band select Outputs on pins 31, 24, 7 and 6. These can be used to select external filters, amplifiers or Transverters. The state of these outputs is defined using the Band Bits setting. 
+
+- The TX output and first three of the Band Select outputs are also available on the Internal Pluto GPO connector. GPO0 is the Tx Output, GPO1-3 are the Band Select outputs.The main use for these is for when the Pluto is remotely mounted. Care must be taken as these pins are low voltage. They will need to be buffered before use. 
+
+To build a complete functional transceiver you will need to add suitable filters, preamplifiers and power amplifiers to the Adalm Pluto. 
+
+All control is done using the touchscreen and mouse.
+
+Tuning uses the mouse scrollwheel. The mouse left and right buttons select the tuning step. The centre button is used for the CW key.  Mouse movement is not used.
+
+A mouse is used to provide the tuning input because it effectively hands the task of monitoring the tuning knob to a seperate processor (in the mouse). Rotary encoders can be tricky to handle reliably in linux. 
+
+It is easy to modify a cheap mouse by disconnecting the existing switches and wiring the PCB to larger switches on the Langstone front panel. The scroll wheel can likewise be replaced with a panel mounted tuning knob. 
+
+Microphone input and headphone output uses the USB audio device. (a couple of pounds on Ebay)
+
+The software consists of three parts. The SDR itself uses two python GNURadio Flowgraphs (Lang_TX.py and Lang_RX.py)which can be created on a PC running GNUradio companion. These Python programs are then manually edited by adding the code from ControlTX.py and ControlRx.py so it can be controlled by the GUI part of the software. This is written in C and communicates with GNURadio using a Linux Pipe. 
+
+
+
+# Installation for Langstone Transceiver
+
+The preferred installation method only needs a Windows PC connected to the same (internet-connected) network as your Raspberry Pi.  Do not connect a keyboard or HDMI display directly to your Raspberry Pi.
+
+- First download the 2020-02-13 release of Raspbian Buster Lite on to your Windows PC from here  https://downloads.raspberrypi.org/raspbian_lite/images/raspbian_lite-2020-02-14/2020-02-13-raspbian-buster-lite.zip
+
+(This version is known to work. Newer versions should also work and may speed up the installation but have not yet been tested) 
+
+- Unzip the image and then transfer it to a Micro-SD Card using Win32diskimager https://sourceforge.net/projects/win32diskimager/
+- Make sure you use a good quality class 10 Micro-SD Card. (16GB is OK) The performance of the Raspberry Pi can be badly affected by poor quality cards. 
+
+- Before you remove the card from your Windows PC, look at the card with windows explorer; the volume should be labeled "boot".  Create a new empty file called ssh in the top-level (root) directory by right-clicking, selecting New, Text Document, and then change the name to ssh (not ssh.txt).  You should get a window warning about changing the filename extension.  Click OK.  If you do not get this warning, you have created a file called ssh.txt and you need to rename it ssh.  IMPORTANT NOTE: by default, Windows (all versions) hides the .txt extension on the ssh file.  To change this, in Windows Explorer, select File, Options, click the View tab, and then untick "Hide extensions for known file types". Then click OK.
+
+- Connect the 7" touchscreen display, USB mouse, USB Sound Card, and Pluto now.   Power up the RPi with the new card inserted, and a network connection.  Do not connect a keyboard or HDMI display to the Raspberry Pi. 
+
+- Find the IP address of your Raspberry Pi using an IP Scanner (such as Advanced IP Scanner http://filehippo.com/download_advanced_ip_scanner/ for Windows, or Fing on an iPhone) to get the RPi's IP address 
+
+- From your windows PC use Putty (http://www.chiark.greenend.org.uk/~sgtatham/putty/download.html) to log in to the IP address that you noted earlier.  You will get a Security warning the first time you try; this is normal.
+
+- Log in (user: pi, password: raspberry) then cut and paste the following code in, one line at a time:
+
+```sh
+wget https://raw.githubusercontent.com/oe5rnl/Langstone-70/master/install.sh
+chmod +x install.sh
+./install.sh
+```
+
+The initial build can take some time, however it does not need any user input, so go and make a cup of coffee and keep an eye on the touchscreen.  When the build is finished the Pi will reboot and start-up with the Langstone Transceiver. If it does not appear to be working see the file 'Debugging Notes.txt' for some things to look at.
+
+# Updating the Software. 
+
+If you have a running Langstone you can update by doing the following. 
+
+Log into the Pi using SSH as described above. 
+
+cd Langstone
+
+./stop
+
+./update
+
+./run
+
+
